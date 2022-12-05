a2d81737a2cc51e4ba70712a1b42f5f9003c7785
Kurre Ståhlberg <kurre@steelmountain.fi>
2022-12-05 09:02:05 +0200

    Days 4 and 5

---
+++ 2022/04/02.py
@@ -0,0 +1,15 @@
+
+def get_range(range: str):
+    return [int(value) for value in range.split("-")]
+
+with open("input.txt") as file:
+    lines = [l.strip() for l in file.readlines()]
+    overlap_count = 0
+    for line in lines:
+        [elf1, elf2] = [get_range(elf) for elf in line.split(",")]
+        if elf1[1] >= elf2[0] and elf1[0] <= elf2[1]:
+            overlap_count += 1
+        elif elf1[1] <= elf2[0] and elf1[0] >= elf2[1]:
+            overlap_count += 1
+
+    print(overlap_count)
---
+++ 2022/05/02.py
@@ -0,0 +1,46 @@
+import math
+import re
+
+def add_stack_layer(stacks: list[list[str]], line: str):
+    stackItems = [line[i: i + 4] for i in range(0, len(line), 4)]
+    if stackItems[0][1].isdigit():
+        return False
+    else:
+        for i in range(len(stackItems)):
+            crate = stackItems[i][1]
+            if crate.isalpha():
+                stacks[i].insert(0, crate)
+    
+    return True
+
+def move_crates(stacks: list[list[str]], line: str):
+    [quantity, source, destination] = re.match("move ([0-9]+) from ([0-9]+) to ([0-9]+)", line).groups()
+
+    quantity = int(quantity)
+    source = int(source) - 1
+    destination = int(destination) - 1
+
+    crates = stacks[source][-quantity:]
+    stacks[source] = stacks[source][:-quantity]
+    stacks[destination].extend(crates)
+
+with open("input.txt") as f:
+    lines = f.readlines()
+    stacking = True
+    stacks: list[list[str]] = []
+    for i in range(math.ceil(len(lines[0]) / 4)):
+        stacks.append([])
+    for line in lines:
+        if len(line.strip()) == 0:
+            continue
+
+        if stacking:
+            stacking = add_stack_layer(stacks, line)
+        else:
+            move_crates(stacks, line)
+
+    result = []
+    for stack in stacks:
+        result.append(stack.pop())
+
+    print("".join(result))
